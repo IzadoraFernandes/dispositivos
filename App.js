7c35ba88f9003a24d28383d66a666c8e7c880d30
IzadoraFernandes <izadorasilvams@gmail.com>
2022-11-07 21:19:32 -0300

    exercicio 03

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.head}>
+
+        <View style={styles.buttom}>
+          <Text style={styles.Textbuttom}>Menu</Text>
+        </View>
+
+        <View style={styles.buttom}>
+          <Text style={styles.Textbuttom}>Logs</Text>
+        </View>
+
+        <View style={styles.buttom}>
+          <Text style={styles.Textbuttom}>Credits</Text>
+        </View>
+
+      </View>
+      <View style={styles.main}>
+        <View>
+          <View></View>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <View>
+          <View><Text>Exercício 03 - DDM</Text></View>
+        </View>
+      </View>
+
+
     </View>
   );
 }
@@ -14,8 +40,53 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#A4AAEA',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+
+  head: {
+    backgroundColor: '#c4c4c4',
+    width: '100%',
+    height: 60,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+
+  },
+
+  main: {
+    width: '100%',
+    flex: 1,
+    height: 60,
+    backgroundColor: '#8A8FC3',
+    
+  },
+
+  footer: {
+
+    backgroundColor: '#4E466A',
+    width: '100%',
+    height: 55,
+
+  },
+
+  buttom: {
+    backgroundColor: 'black',
+    width: 77,
+    height: 28,
+    borderRadius: 10,
+  },
+
+  Textbuttom: {
+    fontSize:16,
+    color: 'white',
+    textAlign: 'center',
+    
+  },
+  Textfooter:{
+    textAlign:'center',
+    fontSize:20
+  }
 });
